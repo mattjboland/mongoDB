22c41cae3754574d0011a5beff02bc3e7e4e1c70
Matt Boland <mattjamesboland@gmail.com>
2020-06-06 13:32:33 +0000

    added mongo_project.py file

--- mongo.py
+++ mongo.py
@@ -22,6 +22,16 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
+new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948',
+            'hair_colour': 'not much', 'occupation': 'writer',
+             'nationality': 'english'}, {'first': 'george',
+            'last': 'rr martin', 'dob': '20/09/1948',
+                                         'hair_colour': 'not much',
+                                         'occupation': 'writer',
+                                         'nationality': 'american'}]
+
+coll.insert_many(new_docs)
+
 documents = coll.find()
 
 for doc in documents:
